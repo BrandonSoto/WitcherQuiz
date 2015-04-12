f6b8507d01c44d5d31d427c1c9851f373ae5dab8
Brandon <brandon.soto09@gmail.com>
2015-04-11 18:40:17 -0700

    Made QuizFragment skeleton

---
+++ app/src/main/java/com/example/brandonwin7/witcherquiz/QuizFragment.java
@@ -0,0 +1,20 @@
+package com.example.brandonwin7.witcherquiz;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class QuizFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_quiz, container, false);
+    }
+
+
+}
