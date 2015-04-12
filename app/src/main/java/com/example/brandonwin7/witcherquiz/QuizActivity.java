b47fb8de8967b1271f2627dee1f6b52d28f4ba1c
Brandon <brandon.soto09@gmail.com>
2015-04-11 19:37:47 -0700

    Finished most of QuizFragment

--- app/src/main/java/com/example/brandonwin7/witcherquiz/QuizActivity.java
+++ app/src/main/java/com/example/brandonwin7/witcherquiz/QuizActivity.java
@@ -1,160 +1,31 @@
 package com.example.brandonwin7.witcherquiz;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class QuizActivity extends Activity {
+public class QuizActivity extends FragmentActivity {
 
     private static final String TAG = QuizActivity.class.getSimpleName();
-    private static final String KEY_INDEX = "index";
+    public static final String KEY_INDEX = "index";
 
-    // gui components
-    private Button mTrueButton;
-    private Button mFalseButton;
-    private Button mNextButton;
-    private Button mPrevButton;
-    private Button mCheatButton;
-    private ImageView mImage;
-    private TextView mQuestionTextView;
-
-    private boolean mIsCheater;
-    private TrueFalse[] mAnswerKey = new TrueFalse[] {
-            new TrueFalse(R.string.q_dandelion, true, R.drawable.dandelion),
-            new TrueFalse(R.string.q_vergen, false, R.drawable.vergen),
-            new TrueFalse(R.string.q_nilfgaard, true, R.drawable.nilfgaard),
-            new TrueFalse(R.string.q_henselt, true, R.drawable.henselt),
-            new TrueFalse(R.string.q_love, false, R.drawable.triss_img)
-    };
-
-    int mCurrentIndex = 0;
-
-    private void updateQuestion(boolean increment) {
-        if (increment) {
-            mCurrentIndex++;
-        } else {
-            mCurrentIndex--;
-        }
-
-        if (mCurrentIndex < 0) {
-            mCurrentIndex = mAnswerKey.length - mCurrentIndex;
-        }
-
-        mCurrentIndex = mCurrentIndex % mAnswerKey.length;
-
-        int question = mAnswerKey[mCurrentIndex].getQuestion();
-        mImage.setImageResource(mAnswerKey[mCurrentIndex].getImageID());
-        mQuestionTextView.setText(question);
-    }
-
-    private void checkAnswer(boolean userPressedTrue) {
-        boolean answerIsTrue = mAnswerKey[mCurrentIndex].isTrueQuestion();
-
-        int messageResId = 0;
-
-        if (mIsCheater) {
-            if (userPressedTrue == answerIsTrue) {
-                messageResId = R.string.judgment_toast;
-            } else {
-                messageResId = R.string.incorrect_judgement_toast;
-            }
-        } else {
-            if (userPressedTrue == answerIsTrue) {
-                messageResId = R.string.correct_toast;
-            } else {
-                messageResId = R.string.incorrect_toast;
-            }
-        }
-
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
-                .show();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        mIsCheater = false;
-
-        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
-
-        mImage = (ImageView)findViewById(R.id.question_image);
-        mImage.setImageResource(mAnswerKey[mCurrentIndex].getImageID());
-        mImage.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-
-
-
-        mTrueButton = (Button)findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAnswer(true);
-                updateQuestion(true);
-            }
-        });
-
-        mFalseButton = (Button)findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAnswer(false);
-                updateQuestion(true);
-            }
-        });
-
-        mNextButton = (Button)findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mIsCheater = false;
-                updateQuestion(true);
-            }
-        });
-
-        mPrevButton = (Button)findViewById(R.id.prev_button);
-        mPrevButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mIsCheater = false;
-                updateQuestion(false);
-            }
-        });
-
-        mCheatButton = (Button)findViewById(R.id.cheat_button);
-        mCheatButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(QuizActivity.this, CheatActivity.class);
-                boolean answerIsTrue = mAnswerKey[mCurrentIndex].isTrueQuestion();
-                i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
-                startActivityForResult(i, 0);
-            }
-        });
-
-        if (savedInstanceState != null) {
-            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null) {
+            fragment = new QuizFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
         }
 
-        updateQuestion(true);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
     @Override
--- app/src/main/java/com/example/brandonwin7/witcherquiz/QuizFragment.java
+++ app/src/main/java/com/example/brandonwin7/witcherquiz/QuizFragment.java
@@ -1,20 +1,164 @@
 package com.example.brandonwin7.witcherquiz;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class QuizFragment extends Fragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_quiz, container, false);
+    // gui components
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private Button mPrevButton;
+    private Button mCheatButton;
+    private ImageView mImage;
+    private TextView mQuestionTextView;
+
+    private boolean mIsCheater;
+    private TrueFalse[] mAnswerKey;
+
+    private int mCurrentIndex = 0;
+
+    public QuizFragment() {
+        super();
+
+        mCurrentIndex = 0;
+        mIsCheater = false;
+        mAnswerKey = new TrueFalse[] {
+                new TrueFalse(R.string.q_dandelion, true, R.drawable.dandelion),
+                new TrueFalse(R.string.q_vergen, false, R.drawable.vergen),
+                new TrueFalse(R.string.q_nilfgaard, true, R.drawable.nilfgaard),
+                new TrueFalse(R.string.q_henselt, true, R.drawable.henselt),
+                new TrueFalse(R.string.q_love, false, R.drawable.triss_img)
+        };
     }
+
+    private void updateQuestion(boolean increment) {
+        if (increment) {
+            mCurrentIndex++;
+        } else {
+            mCurrentIndex--;
+        }
+
+        if (mCurrentIndex < 0) {
+            mCurrentIndex += mAnswerKey.length;
+        }
+
+        mCurrentIndex = mCurrentIndex % mAnswerKey.length;
+
+        Log.d("FRAGMENT", "index: " + mCurrentIndex);
+
+        int question = mAnswerKey[mCurrentIndex].getQuestion();
+        mImage.setImageResource(mAnswerKey[mCurrentIndex].getImageID());
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(Context parent, boolean userPressedTrue) {
+        boolean answerIsTrue = mAnswerKey[mCurrentIndex].isTrueQuestion();
+
+        int messageResId = 0;
+
+        if (mIsCheater) {
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.judgment_toast;
+            } else {
+                messageResId = R.string.incorrect_judgement_toast;
+            }
+        } else {
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
+        }
+
+        Toast.makeText(parent, messageResId, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        final View v = inflater.inflate(R.layout.fragment_quiz, parent, false);
+        final Context pContext = parent.getContext();
+
+
+        mQuestionTextView = (TextView) v.findViewById(R.id.question_text_view);
+
+        mImage = (ImageView)v.findViewById(R.id.question_image);
+        mImage.setImageResource(mAnswerKey[mCurrentIndex].getImageID());
+        mImage.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+
+
+        mTrueButton = (Button)v.findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(pContext, true);
+                updateQuestion(true);
+            }
+        });
+
+        mFalseButton = (Button)v.findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(pContext, false);
+                updateQuestion(true);
+            }
+        });
+
+        mNextButton = (Button)v.findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIsCheater = false;
+                updateQuestion(true);
+            }
+        });
+
+        mPrevButton = (Button)v.findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIsCheater = false;
+                updateQuestion(false);
+            }
+        });
+
+        mCheatButton = (Button)v.findViewById(R.id.cheat_button);
+
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(QuizActivity.KEY_INDEX, 0);
+        }
+
+        updateQuestion(true);
+        return v;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(QuizActivity.KEY_INDEX, mCurrentIndex);
+    }
+
 
 
 }
